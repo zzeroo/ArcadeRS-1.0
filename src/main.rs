f969afa0064178d2bc78d586528086ad8fa82be6
Stefan Müller <s.mueller@it.kls-glt.de>
2017-03-25 07:01:58 +0100

    1. A simple window

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+extern crate sdl2;
+
+use sdl2::pixels::Color;
+use std::thread;
+use std::time::Duration;
+
+
+fn main() {
+    // Initalize sdl2
+    let sdl_context = sdl2::init().unwrap();
+    let video = sdl_context.video().unwrap();
+
+    // Create the window
+    let window = video.window("ArcardeRS Shooter", 800, 600)
+        .position_centered().opengl()
+        .build().unwrap();
+
+    let mut renderer = window.renderer()
+        .accelerated()
+        .build().unwrap();
+
+    // Redner a fully black window
+    renderer.set_draw_color(Color::RGB(0, 0, 0));
+    renderer.clear();
+    renderer.present();
+
+    thread::sleep(Duration::from_millis(3000));
+}
